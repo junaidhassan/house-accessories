8576fc6232ae90fa1639cabc56c34f6047a26b12
Muzammil Hussain <muzammil.hussain@tkxel.com>
2013-09-25 17:21:07 +0500

    Add sort arrows.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,17 @@
+# encoding: utf-8
 module ApplicationHelper
 
   def sortable(column, title = nil)
     title ||= column.to_s.titleize
-    css_class = column == sort_column ? "current #{sort_direction}" : nil
-    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    #css_class = column.to_s == sort_column ? "current #{sort_direction}" : nil
+    direction = column.to_s == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
     params.delete('_')
-    link_to title, params.merge(:sort => column, :direction => direction, :page => nil), {:class => css_class}
+    sortArrow = column.to_s == sort_column ? (direction == 'asc' ? '▲' : '▼') : nil
+    link_to "#{title} #{sortArrow}", params.merge(:sort => column, :direction => direction, :page => nil)#, {:class => css_class}
+  end
+
+  def sort_css_class(column)
+    #column.to_s == sort_column ? "current #{sort_direction}" : nil
   end
 
 end
