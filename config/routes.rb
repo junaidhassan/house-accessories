45b364aa50612d94112ce9313a24626adea17e28
Muzammil Hussain <muzammil.hussain@tkxel.com>
2013-09-25 15:27:44 +0500

    Little change

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ HouseAccessories::Application.routes.draw do
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all  your routes lay out with "rake routes".
   get 'save-results' => 'items#save_results', as: :save_results
   get 'upload' => 'items#new_import', as: :upload
   # You can have the root of your site routed with "root"
